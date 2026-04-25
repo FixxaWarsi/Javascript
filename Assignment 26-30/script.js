72dc5fe7949979f30333d3d4c87cdd186cada14d
FixxaWarsi <fixxa.warsi@gmail.com>
2026-04-25 16:57:52 +0500

    Add files via upload

---
+++ Assignment 26-30/script.js
@@ -0,0 +1,69 @@
+// 1. Positive Integer Rounding
+// var posNum = +prompt("Enter a positive integer:");
+
+// console.log("Number: " + posNum);
+// console.log("Round off value: " + Math.round(posNum));
+// console.log("Floor value: " + Math.floor(posNum));
+// console.log("Ceil value: " + Math.ceil(posNum));
+
+
+// 2. Negative Floating Number
+// var negNum = +prompt("Enter a negative floating point number:");
+
+// console.log("Number: " + negNum);
+// console.log("Round off value: " + Math.round(negNum));
+// console.log("Floor value: " + Math.floor(negNum));
+// console.log("Ceil value: " + Math.ceil(negNum));
+
+
+// 3. Absolute Value
+// var absNum = +prompt("Enter any number to find absolute value:");
+
+// console.log("The absolute value of " + absNum + " is: " + Math.abs(absNum));
+
+
+// 4. Dice Simulation
+// var dice = Math.floor(Math.random() * 6) + 1;
+
+// console.log("Random dice value: " + dice);
+
+
+// 5. Coin Toss Simulation
+// var toss = Math.floor(Math.random() * 2) + 1;
+
+// if (toss === 1) {
+//     console.log("Random coin value: Heads");
+// } else {
+//     console.log("Random coin value: Tails");
+// }
+
+
+// 6. Random Number (1 to 100)
+// var randomNum = Math.floor(Math.random() * 100) + 1;
+
+// console.log("Random number between 1 and 100: " + randomNum);
+
+
+// 7. Weight Parsing Program
+// var weightInput = prompt("Enter your weight (e.g., 50, 50kgs, 50.2kgs, 50.2kilograms):");
+// var weight = parseFloat(weightInput);
+
+// console.log("Your weight is: " + weight + " kilograms");
+
+
+// EXTRA PRACTICE (Conversions & toFixed)
+
+// String to Number
+// var strNum = "123.456";
+// var numConverted = Number(strNum);
+// console.log("Converted to number: " + numConverted);
+
+// Number to String
+// var num = 789;
+// var strConverted = String(num);
+// console.log("Converted to string: " + strConverted);
+
+// Controlling decimal length
+// var decimalNum = 12.34567;
+// console.log("Original: " + decimalNum);
+// console.log("Rounded to 2 decimal places: " + decimalNum.toFixed(2));
